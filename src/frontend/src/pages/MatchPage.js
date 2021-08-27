1fc14f344e4c6de56eafa47bf664dfa56132997a
reet <reet@192.168.1.214>
2021-08-26 22:51:16 -0400

    MatchPage year selector and HomePage

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,15 +1,15 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
+import { YearSelector } from '../components/YearSelector';
 
-
+import './MatchPage.scss';
 export const MatchPage = () => {
 
-const [matches, setMatches] = useState([]);
-const { teamName, year} = useParams();
+	const [matches, setMatches] = useState([]);
+	const { teamName, year } = useParams();
 
-useEffect(
+	useEffect(
 
 		() => {
 			const fetchMatches = async () => {
@@ -21,16 +21,24 @@ useEffect(
 			};
 
 			fetchMatches();
-		}, []
+		}, [teamName, year]
 	);
 
 
 	return (
 		<div className="MatchPage">
-		<h1>Match Page</h1>
-		{
-			matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
-		}
+		
+			<div className="year-selector">
+			<h3>Select Year</h3>
+               <YearSelector teamName={teamName}/>
+			</div>
+
+			<div>
+				<h1 className="page-heading">{teamName} matches in {year}</h1>
+				{
+					matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+				}
+			</div>
 		</div>
 	);
 }
